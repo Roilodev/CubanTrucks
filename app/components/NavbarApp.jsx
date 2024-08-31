dc99f584df2fa5651de27bb01b207d2533d11377
Roilodev <156539959+Roilodev@users.noreply.github.com>
2024-08-31 19:41:10 -0400

    Primera parte del formulario de nueva oferta

--- app/components/NavbarApp.jsx
+++ app/components/NavbarApp.jsx
@@ -1,62 +1,119 @@
 "use client";
-import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu } from "@nextui-org/react";
-import { ChevronDown, Lock, Activity, Flash, Server, TagUser, Scale } from "../icons";
+import React, {useState} from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from "@nextui-org/react";
+import {
+  ChevronDown,
+  Lock,
+  Activity,
+  Flash,
+  Server,
+  TagUser,
+  Scale,
+} from "../icons";
 import { AcmeLogo } from "../AcmeLogo.jsx";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Button, Checkbox, Input, DatePicker, Textarea } from "@nextui-org/react";
-import { MailIcon } from '../MailIcon';
-import { LockIcon } from '../LockIcon.jsx';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Button,
+  Input,
+  DatePicker,
+  Textarea,
+} from "@nextui-org/react";
 
 export default function App() {
   const icons = {
     chevron: <ChevronDown fill="currentColor" size={16} />,
     scale: <Scale className="text-warning" fill="currentColor" size={30} />,
     lock: <Lock className="text-success" fill="currentColor" size={30} />,
-    activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
+    activity: (
+      <Activity className="text-secondary" fill="currentColor" size={30} />
+    ),
     flash: <Flash className="text-primary" fill="currentColor" size={30} />,
     server: <Server className="text-success" fill="currentColor" size={30} />,
     user: <TagUser className="text-danger" fill="currentColor" size={30} />,
   };
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
+  const handleSubmit = (e) => {
+    console.log(e.target);
+  }
+
   return (
     <>
-      <Modal
-        size="lg"
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        placement="top-center"
-        classNames={{
-          backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
-        }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Creando Oferta para Transportación</ModalHeader>
-              <ModalBody>
-                <div className="grid lg:grid-cols-2 gap-4">
-                  <Input className="lg:col-span-2" type="text" label="Mercancía a transportar" />
-                  <Input type="number" label="Peso aprox. en Kgs" />
-                  <Input type="number" label="Volumen aprox. en m3" />
-                  <Input type="number" label="Precio de oferta" color="success" />
-                  <DatePicker label="Fecha de Transportación" className="max-w-[284px]" />
-                  <Textarea label="Comentario" className="lg:col-span-2" />
-                </div>
-
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Cancelar
-                </Button>
-                <Button color="success" onPress={onClose}>
-                  Publicar
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      <form onSubmit={handleSubmit}>
+        <Modal
+          size="lg"
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          placement="top-center"
+          classNames={{
+            backdrop:
+              "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
+          }}
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Creando Oferta para Transportación
+                </ModalHeader>
+                <ModalBody>
+                  <div className="grid lg:grid-cols-2 gap-4">
+                    <Input
+                      className="lg:col-span-2"
+                      type="file"
+                      label="Foto de la mercancía"
+                      name="photo"
+                    />
+                    <Input
+                      className="lg:col-span-2"
+                      type="text"
+                      label="Mercancía a transportar"
+                      name="title"
+                    />
+                    <Input type="number" label="Peso aprox. en Kgs" />
+                    <Input type="number" label="Volumen aprox. en m3" />
+                    <Input
+                      type="number"
+                      label="Precio de oferta"
+                      color="success"
+                      name="price"
+                    />
+                    <DatePicker
+                      label="Fecha de Transportación"
+                      className="max-w-[284px]"
+                      name="transpDate"
+                    />
+                    <Textarea label="Comentario" className="lg:col-span-2" />
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="flat" onPress={onClose}>
+                    Cancelar
+                  </Button>
+                  <Button color="success" onPress={onClose} type="submit">
+                    Publicar
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </form>
       <Navbar>
         <NavbarBrand>
           <AcmeLogo />
@@ -122,9 +179,10 @@ export default function App() {
             </DropdownMenu>
           </Dropdown>
         </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-
-        </NavbarContent>
+        <NavbarContent
+          className="hidden sm:flex gap-4"
+          justify="center"
+        ></NavbarContent>
         <NavbarContent justify="end">
           <NavbarItem className="hidden lg:flex">
             <Link href="#">Registrarse</Link>
@@ -137,6 +195,5 @@ export default function App() {
         </NavbarContent>
       </Navbar>
     </>
-
   );
 }
